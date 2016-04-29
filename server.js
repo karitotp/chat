704c668f1f89bcb542e34aac9feb68a19c74b9ed
Rub21 <ruben@mapbox.com>
2016-04-29 16:19:00 -0500

    add

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var socketio = require('socket.io');
-var express = require('express');
- 
-var server = express(); // better instead
-  server.use(express.static(__dirname + '/public'));
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+
+    
+  });
+
+  socket.on('location', function(position) {
+    console.log(position);
+    io.emit('location',position);
+  });
+});
 
 
-server.listen(8080);
 
-
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
