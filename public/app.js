78772dd8987e4d1c46cd419e96fea8ead6abb246
karitotp <karito@mapbox.com>
2016-05-08 23:00:42 -0500

    fix chat v1

--- public/app.js
+++ public/app.js
@@ -1,66 +1,79 @@
-var socket = null;
-var user = null;
-$(document).ready(function() {
-    $("#layermapa").hide();
-    $("#btnJoin").click(function() {
-        socket = io.connect('http://localhost:8080'); //iniciamos conexion con el server
-        user = $("#user").val(); //guardamos el usuario insertado en memoria
-        //socket.on('login',user);//llamamos el evento login del server
+          L.mapbox.accessToken = 'pk.eyJ1Ijoia2FyaXRvdHAiLCJhIjoib0ppeVhnWSJ9.JOLM9BQLqtI_bjvNzjNPew';
+          var geolocate = document.getElementById('geolocate');
+          var map = L.mapbox.map('map', 'mapbox.streets');
 
-        socket.on('message', function(msg) {
-            console.log('Received Message: %s', msg);
-            //$(".messages").append(msg.text);
-            $(".marker-title").append(msg.text);
-        });
-        $("#layermapa").show();
-        $(".user").hide();
-    });
-    $('form').submit(function() {
-        socket.emit('message', {
-            "user": user,
-            "message": $("#textSend").val()
-        });
-        $('#textSend').val('');
-        return false;
-    });
-    $('#textSend').keyup(function(evt) {
-        if (evt.keyCode === 13) {
-            $('textSend').val('');
-        }
-    });
-});
+          var myLayer = L.mapbox.featureLayer().addTo(map);
+          if (!navigator.geolocation) {
+            geolocate.innerHTML = 'Geolocation is not available';
+          } else {
+            geolocate.onclick = function(e) {
+              e.preventDefault();
+              e.stopPropagation();
+              map.locate();
+            };
+          }
 
-L.mapbox.accessToken = 'pk.eyJ1Ijoia2FyaXRvdHAiLCJhIjoib0ppeVhnWSJ9.JOLM9BQLqtI_bjvNzjNPew';
-var geolocate = document.getElementById('geolocate');
-var map = L.mapbox.map('map', 'mapbox.streets');
-var myLayer = L.mapbox.featureLayer().addTo(map); //api html5
-if (!navigator.geolocation) {
-    geolocate.innerHTML = 'Geolocation is not available';
-} else {
-    geolocate.onclick = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        map.locate();
-    };
-}
-//muestra posicion en el mapa
-map.on('locationfound', function(e) {
-    map.fitBounds(e.bounds);
-    myLayer.setGeoJSON({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [e.latlng.lng, e.latlng.lat]
-        },
-        properties: {
-            'title': '' + user,
-            'marker-color': '#ff8888',
-            'marker-symbol': 'star'
-        }
+          map.on('locationfound', function(e) {
+
+            map.fitBounds(e.bounds);
+            var position = {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [e.latlng.lng, e.latlng.lat]
+              },
+              properties: {
+                'title': ''+user,
+                'marker-color': '#ff8888',
+                'marker-symbol': 'star'
+              }
+            };
+
+            socket.emit('location', position);
+
+            myLayer.setGeoJSON(position);
+            myLayer.openPopup();
+            geolocate.parentNode.removeChild(geolocate);
+
+
+          });
+          map.on('locationerror', function() {
+            geolocate.innerHTML = 'Position could not be found';
+          });
+
+          //var socket = null;
+          //var user = null;
+
+    $(document).ready(function(){
+    $(".layermapa").hide();
+    $("#btnJoin").click(function(){
+    socket = io.connect('http://localhost:3000');//iniciamos conexion con el server
+    user = $("#user").val();//guardamos el usuario insertado en memoria
+    socket.emit('login',user);//llamamos el evento login del server
+    socket.on('chat_message', function (msg) {
+    console.log('Received Message: %s', msg);
+    //$(".messages").append(msg.text);
+    $(".marker-title").append(msg.text);
     });
-    myLayer.openPopup();
-    geolocate.parentNode.removeChild(geolocate);
-});
-map.on('locationerror', function() {
-    geolocate.innerHTML = 'Position could not be found';
-});
+    $(".layermapa").show();
+    $(".user").hide();
+    });
+    $('form').submit(function(){
+    socket.emit('chat_message',{"user": user,"chat_message": $("#m").val()});
+    $('#m').val('');
+    return false;
+    });
+    $('#m').keyup(function (evt) {
+    if (evt.keyCode === 13) {
+    $('m').val('');
+    }
+    });
+    });
+
+           //==========================================================================
+          socket.on('location', function(position) {
+            console.log(position);
+            myLayer.setGeoJSON(position);
+
+
+          });
--- server.js
+++ server.js
@@ -1,10 +1,47 @@
-var socketio = require('socket.io');
+
 var express = require('express');
- 
-var server = express(); // better instead
-  server.use(express.static(__dirname + '/public'));
+var app = express();
+var path = require('path');   
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
 
 
-server.listen(8080);
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 
+var users = [];
+
+io.on('connection', function(socket) {
+
+  socket.on('login', function(msg) {
+    console.log('Usuario conectado: %s', msg);
+    socket.emit('login', {
+      "user": "System",
+      "text": "Welcome to the chat"
+    });
+    users.push(msg);
+  });
+
+
+
+  socket.on('chat_message', function(msg) {
+  	var text = '<p class="chat_message"><b>' + msg.user + '</b>:' + msg.chat_message + '</p>';
+    io.emit('chat_message', {
+      "user": msg.user,
+      "text": text
+    });
+
+    
+  });
+
+  socket.on('location', function(position) {
+    console.log(position);
+    io.emit('location',position);
+  });
+});
+
+
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
